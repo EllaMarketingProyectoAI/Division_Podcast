1bbe667e6159d8b01e1175d61f8dfceff47ee1b7
EllaMarketingProyectoAI <proyectoai@ella.marketing>
2025-08-01 14:13:14 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify
 from supabase_upload import procesar_video_y_subir
-from ffmpeg_split import split_and_upload
+from ffmpeg_split import dividir_video_en_segmentos
 
 app = Flask(__name__)
 
