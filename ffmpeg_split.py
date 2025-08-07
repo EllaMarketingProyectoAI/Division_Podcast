0c1ff28e0aedc4d9994f840fa70f4d29e5c68d97
EllaMarketingProyectoAI <proyectoai@ella.marketing>
2025-08-07 12:19:21 -0600

    Update ffmpeg_split.py

--- ffmpeg_split.py
+++ ffmpeg_split.py
@@ -34,6 +34,15 @@ def dividir_video_en_segmentos(input_path, output_dir, base_output_name, duracio
             "-vn", "-acodec", "libmp3lame", "-q:a", "2",
             mp3_output, "-y"
         ]
+        comando_mp4 = [
+            "ffmpeg", "-ss", str(inicio), "-i", input_path,
+            "-t", str(duracion_segmento),
+            "-c:v", "libx264", "-preset", "veryfast",
+            "-crf", "28",
+            "-c:a", "aac", "-b:a", "128k",
+            "-movflags", "+faststart",  # ⚠️ Agregado para permitir reproducción por streaming
+            mp4_output, "-y"
+        ]
         subprocess.run(comando_mp3, check=True)
 
         output_paths.append((mp4_output, mp3_output))
